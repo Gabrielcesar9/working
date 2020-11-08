c2d6965acf6287c3dba00d5404fbfca1f67e2d94
Gabriel <gabriel.cesar.pereira.9@gmail.com>
2020-11-08 14:41:45 -0300

    tryout

--- src/try.js
+++ src/try.js
@@ -14,7 +14,7 @@ exports.handler = function(event, context, callback) {
     view: "Grid view"
 }).eachPage(function page(records, fetchNextPage) {
     records.forEach(function(record) {
-        cpfs.push(record);
+        cpfs.push(record.get('CPF'));
         console.log('Retrieved', record.get('CPF'));
     });
     fetchNextPage();},
