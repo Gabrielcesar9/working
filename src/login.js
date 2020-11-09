ccbdf59bf0730259b97d608c789714d8bc7170e9
Gabriel <gabriel.cesar.pereira.9@gmail.com>
2020-11-09 12:21:06 -0300

    tryout

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 const Airtable = require ('airtable');
-exports.handler = async function(event, context, callback) {
+exports.handler = function(event, context, callback) {
     console.log(typeof(event.body));
     const {API_URL, API_CLIENT_ID, API_KEY } = process.env;
   var base = new Airtable({apiKey:API_KEY}).base(
@@ -11,25 +11,22 @@ exports.handler = async function(event, context, callback) {
 
   item = event.body;
   console.log('item', item);
-  var cpfs = new Array();
-  base('Table 2').select({
-        // Selecting the first 3 records in Grid view:
-        maxRecords: 100,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records. 
-        records.forEach(function(record) {
-          cpfs.push(record.get('CPF'));
-            console.log('Retrieved', record.get('CPF'));
+  function func(){
+      let cpfs = new Array();
+        base('Table 2').select({
+            view: 'Grid view'
+        }).eachPage(function(err, records) {
+            if (err) { console.error(err); return;}
+            records.forEach(function(record) {
+                console.log('tyoeof', typeof(record.get('Nome')));
+                cpfs.push(record.get('Nome'));
+            });
         });
-        fetchNextPage();
-    
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
-  setTimeout(function(){console.log('cpfs',cpfs);
-  if(cpfs.includes(item)){console.log("já existe");alert("Este CPF já está cadastrado")}},
-   3000);}
+        return cpfs;
+  }
+  let cpfs2 = func();
+  console.log('cpfs2:', cpfs2);
+  setTimeout(function(){console.log('cpfs:', cpfs)}, 3000);}
   
   /**
     AIRTABLE REQUEST LOGIC GOES HERE, APPENDING TO DATA
