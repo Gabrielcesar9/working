f1d475219d8ee5838cbd6239e51a93bb10174358
Gabriel <gabriel.cesar.pereira.9@gmail.com>
2020-11-09 11:17:28 -0300

    tryout

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 const Airtable = require ('airtable');
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
     console.log(typeof(event.body));
     const {API_URL, API_CLIENT_ID, API_KEY } = process.env;
   var base = new Airtable({apiKey:API_KEY}).base(
@@ -11,12 +11,14 @@ exports.handler = function(event, context, callback) {
 
   item = event.body;
   console.log('item', item);
+  let promise = new Promise(resolve =>{
   var cpfs = new Array();
-      base('Table 2').select({
+  base('Table 2').select({
         // Selecting the first 3 records in Grid view:
         maxRecords: 100,
         view: "Grid view"
     }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records. 
         records.forEach(function(record) {
           cpfs.push(record.get('Nome'));
             console.log('Retrieved', record.get('Nome'));
@@ -26,7 +28,12 @@ exports.handler = function(event, context, callback) {
     }, function done(err) {
         if (err) { console.error(err); return; }
     });
-  setTimeout(function(){console.log('cpfs:', cpfs)}, 3000);}
+    ()=>resolve(cpfs);
+  })
+  var cpfs2 = await promise;
+  console.log('cpf2', cpf2);
+  //setTimeout(function(){console.log('cpfs:', cpfs)}, 3000);
+}
   
   /**
     AIRTABLE REQUEST LOGIC GOES HERE, APPENDING TO DATA
