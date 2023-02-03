42d3509e459b6a29a8acd8eb536ac2394a897578
Gabriel <gabriel.cesar.pereira.9@gmail.com>
2023-02-03 13:38:35 -0300

    removing rg from function

--- src/try.js
+++ src/try.js
@@ -14,22 +14,22 @@ base('prematricula').create([{
       "fields":{
           "Nome":items[0],
           "CPF":items[1],
-          "RG":items[2],
-          "Data de nascimento":items[3],
-          "Endereco":items[4],
-          "Telefone do aluno":items[5],
-          "E-mail do aluno":items[6],
-          "Serie ou ano em 2022 (na escola)":items[7],
-          "Periodo (manha, tarde, noite)":items[8],
-          "Nome do pai/mae/tutor responsavel":items[9],
-          "CPF2":items[10],
-          "RG2":items[11],
-          "Data de nascimento2":items[12],
-          "Endereco2":items[13],
-          "Telefone2":items[14],
-          "E-mail2":items[15],
-          "Profissao":items[16],
-          "Outrotelefonedecontato":items[17]
+          //"RG":items[2],
+          "Data de nascimento":items[2],
+          "Endereco":items[3],
+          "Telefone do aluno":items[4],
+          "E-mail do aluno":items[5],
+          "Serie ou ano em 2023 (na escola)":items[6],
+          "Periodo (manha, tarde, noite)":items[7],
+          "Nome do pai/mae/tutor responsavel":items[8],
+          "CPF2":items[9],
+          //"RG2":items[11],
+          "Data de nascimento2":items[10],
+          "Endereco2":items[11],
+          "Telefone2":items[12],
+          "E-mail2":items[13],
+          "Profissao":items[14],
+          "Outrotelefonedecontato":items[15]
 
       }
   }], function(err, records){
